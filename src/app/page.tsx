7b14ebfb26f1509d08289a4c0a77a8560a179f14
Kartik Kochhar <kochharkartik51@gmail.com>
2024-09-06 09:33:50 +0530

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     <h1>Spotify Pet Finder</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#000000] to-[#060714] text-white">
+     <h1 className="text-4xl cursor-pointer">Sign In</h1>
     </main>
   );
 }
