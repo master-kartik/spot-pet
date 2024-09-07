83efa13b36621b452b7a187d5a0288ca15e973c8
Kartik Kochhar <kochharkartik51@gmail.com>
2024-09-07 08:58:26 +0530

    added spotify authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "~/styles/globals.css";
-
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
@@ -13,8 +13,10 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
       <body>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 export const dynamic = "force-dynamic"
 import { db } from "~/server/db";
@@ -5,14 +6,22 @@ export default async function HomePage() {
   const users =await db.query.users.findMany()
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#000000] to-[#060714] text-white">
-     <h1 className="text-4xl cursor-pointer">Sign In</h1>
-      {([...users]).map((user, index)=>(
+     <h1 className="text-4xl cursor-pointer">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+     </h1>
+
+      {/* {([...users]).map((user, index)=>(
           <div key={index}>
             <div>Hello</div>
             {user.name}
           
           </div>
-      ))}
+      ))} */}
     </main>
   );
 }
