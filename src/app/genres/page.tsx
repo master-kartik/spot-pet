d53094d6158b424daeb1ab3594884c6967aa2369
Kartik Kochhar <kochharkartik51@gmail.com>
2024-10-15 08:57:19 +0530

    desktop app ready

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -1,142 +1,12 @@
-// "use client";
-// import { useAuth } from "@clerk/nextjs";
-// import axios from "axios";
-// import { useState, useEffect, useRef } from "react";
-// import getPhotosByQuery from "../api/fetchImage/route";
-// import { MultiStepLoaderDemo } from "../_components/MultiStepLoader";
-// import html2canvas from 'html2canvas';
-
-// interface PetNames {
-//   breed: string;
-//   pet: string;
-//   reasoning: string;
-// }
-// const Genres = () => {
-//   const { isSignedIn } = useAuth();
-//   const [genres, setGenres] = useState<string[]>([]);
-//   const [petNames, setPetNames] = useState<PetNames | null>(null);
-//   const [error, setError] = useState<string | null>(null);
-//   const [photos, setPhotos] = useState<any[]>([]);
-//   const [loading, setLoading] = useState(false);
-//   const petRef = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     const fetchClientGenres = async () => {
-//       if (!isSignedIn) return;
-//       setLoading(true);
-//       try {
-//         const response = await fetch("/api/spotify/genres");
-//         const data = await response.json();
-
-//         if (response.ok) {
-//           // console.log("Fetched genres:", data);
-//           setGenres(data);
-//           await suggestPetName(data);
-          
-//         } else {
-//           setError(data.message || "Failed to fetch Spotify profile");
-//         }
-//       } catch (err) {
-//         setError("An error occurred while fetching Spotify profile");
-//       }
-//       finally {
-//         setLoading(false); 
-//       }
-
-//     };
-
-//     fetchClientGenres();
-   
-//   }, [isSignedIn]);
-
-//   const suggestPetName = async (genres: string[]) => {
-//     setLoading(true);
-//     await axios.post('/api/suggestPet',{
-//       genres: genres
-//     }, {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer ACCESS_TOKEN'
-//       }
-//     })
-//     .then(async response => {
-//       // console.log(response.data);
-//       const candidates = response.data.candidates[0].content.parts[0].text;
-//       // console.log(candidates)
-
-//       const parsedPetNames: PetNames = JSON.parse(candidates);
-//     // console.log("Parsed Pet Names:", parsedPetNames);
-//     if (parsedPetNames.pet) {
-//       const photoData = await getPhotosByQuery({ query: parsedPetNames.pet });
-//       setPhotos(photoData.results); 
-//       console.log(photos)
-//     }
-//     setPetNames(parsedPetNames);
-   
-
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     })
-//     .finally(() => {
-//       setLoading(false); 
-//     });
-//   }
-
-
-//   const handleRegenerate = () => {
-//     if (genres.length > 0) {
-//       suggestPetName(genres); 
-//     }
-//   };
-
-//   return (
-//     <main  className="min-h-screen mt-0 bg-[#f9f9f9]">
-//        <div className="flex flex-col my-20 px-36 items-start justify-center">
-//        {loading ? (
-//           <MultiStepLoaderDemo /> 
-//         ) :
-//        petNames ? (
-//         <>
-//         {photos.length >= 0 && (
-//              <img className="h-[60vh] object-cover w-[80vw]" src={photos[0]?.urls.full} alt={petNames.pet} />
-//            )}
-         
-//       <section className="flex flex-col space-y-4 my-20 w-[90%]">
-//         <h1 className="text-6xl mb-4 font-bold italic tracking-tighter text-black">
-//           {petNames?.pet}
-//         </h1>
-//         <div className="w-[30vw] h-[2px] bg-black"></div>
-//         <p className="font-inter text-lg leading-6 tracking-tight">
-//         {petNames?.reasoning}
-//         </p>
-//       </section>
-//       <div className="flex gap-5">
-//       <button onClick={handleShare} className="bg-secondary text-background py-3 px-6  hover:bg-secondary-focus transition duration-300">
-//         Share
-//         </button>
-//         <button className="bg-white text-secondary border-2 border-secondary py-3 px-6  " onClick={handleRegenerate}>
-//         Regenerate
-//         </button>
-//       </div>
-//         </>)
-//        :(<div></div>)}
-//        </div>
-//     </main>
-//   );
-
-// };
-
-// export default Genres;
-
-
 "use client";
-import { useAuth } from "@clerk/nextjs";
+import { RedirectToSignUp, SignedIn, SignedOut, useAuth } from "@clerk/nextjs";
 import axios from "axios";
+import { ShareSocial } from 'react-share-social';
 import { useState, useEffect, useRef } from "react";
 import getPhotosByQuery from "../api/fetchImage/route";
 import { MultiStepLoaderDemo } from "../_components/MultiStepLoader";
 import html2canvas from 'html2canvas';
+import { permanentRedirect } from "next/navigation";
 
 interface PetNames {
   breed: string;
@@ -151,8 +21,9 @@ const Genres = () => {
   const [error, setError] = useState<string | null>(null);
   const [photos, setPhotos] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
+
   
-  // Create a ref for capturing the pet details
+
   const petRef = useRef<HTMLDivElement>(null); 
 
   useEffect(() => {
@@ -194,7 +65,7 @@ const Genres = () => {
 
       if (parsedPetNames.pet) {
         const photoData = await getPhotosByQuery({ query: parsedPetNames.pet });
-        setPhotos(photoData.results); // Set the fetched photos
+        setPhotos(photoData.results); 
       }
       setPetNames(parsedPetNames);
     } catch (error) {
@@ -203,6 +74,7 @@ const Genres = () => {
       setLoading(false); 
     }
   }
+
 
   // Function to handle sharing
   const handleShare = async () => {
@@ -216,24 +88,25 @@ const Genres = () => {
       }
 
       try {
-        // Capture the screenshot with allowTaint option
         const canvas = await html2canvas(petRef.current, { 
           allowTaint: true,
           useCORS: true,
+          scale: 5,
          });
 
         canvas.toBlob(async (blob) => {
           if (blob) {
             const file = new File([blob], "pet.png", { type: "image/png" });
+            
 
-            // Check if Web Share API is supported
+            
             if (navigator.share) {
               try {
+                await navigator.clipboard.writeText(`hehe found my spirit animal using pettospot this is based on my music taste 👀, check yours? \n ${window.location.href}`);
+                // alert('Hey we have copied somthing beautiful in your clipboard please paste that while sharing ')
                 await navigator.share({
-                  title: `Check out my pet name: ${petNames?.pet}`,
-                  text: `I got a new pet name: ${petNames?.reasoning}`,
+                  text: 'hehe found my spirit animal using pettospot this is based on my music taste 👀',
                   files: [file],
-                  url: window.location.href,
                 });
               } catch (err) {
                 console.error("Error sharing:", err);
@@ -252,13 +125,15 @@ const Genres = () => {
   };
 
   return (
-    <main className="min-h-screen mt-0 bg-[#f9f9f9] ">
+    <>
+    <SignedIn>
+      <main className="min-h-screen mt-0 bg-[#f9f9f9] ">
       <div className="flex flex-col my-20 px-36 items-start justify-center" >
         {loading ? (
           <MultiStepLoaderDemo /> 
         ) : petNames ? (
           <>
-          <div className="py-4 px-8" ref={petRef}>
+          <div className="py-4 px-16" ref={petRef}>
             {photos.length > 0 && (
               <img 
                 className="h-[60vh] object-cover w-[80vw]" 
@@ -271,7 +146,7 @@ const Genres = () => {
               <h1 className="text-6xl mb-4 font-bold italic tracking-tighter text-black">
                 {petNames?.pet}
               </h1>
-              <div className="w-[30vw] h-[2px] bg-black"></div>
+              <div className="w-[30vw] h-[2px] mt-4 bg-black"></div>
               <p className="font-inter text-lg leading-6 tracking-tight">
                 {petNames?.reasoning}
               </p>
@@ -280,16 +155,17 @@ const Genres = () => {
             <div className="flex gap-5">
               <button 
                 className="bg-secondary text-background py-3 px-6 hover:bg-secondary-focus transition duration-300"
-                onClick={handleShare} // Attach share handler
+                onClick={handleShare} 
               >
                 Share
               </button>
               <button 
                 className="bg-white text-secondary border-2 border-secondary py-3 px-6"
-                onClick={() => suggestPetName(genres)} // Regenerate functionality
+                onClick={() => suggestPetName(genres)} 
               >
                 Regenerate
               </button>
+
             </div>
           </>
         ) : (
@@ -297,6 +173,11 @@ const Genres = () => {
         )}
       </div>
     </main>
+    </SignedIn>
+    <SignedOut>
+    {permanentRedirect(`/`)}
+    </SignedOut>
+    </>
   );
 };
 
